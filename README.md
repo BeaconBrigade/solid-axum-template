89ff488fcca8af771a473f0c04f6fd153be45243
BeaconBrigade <BeaconBrigade@gmail.com>
2022-07-09 18:59:47 -0230

    Updated README and fixed build script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # solid-js and axum-rs template
 
-This repo is for creating [solid-js](https://github.com/tokio-rs/axum) apps served using [axum-rs](https://github.com/solidjs/solid).
+This repo is for creating [solid-js](https://github.com/tokio-rs/axum) apps served using [axum-rs](https://github.com/solidjs/solid). *You will still have to manually run `yarn install`.
 
 The `build.rs` file will remake the solid app anytime you modify files within `frontend/`. Axum is set up to serve the default solid app.
--- build.rs
+++ build.rs
@@ -1,11 +1,27 @@
 use std::process::Command;
+use std::io::{self, Write};
 
 // rebuild front-end
 fn main() {
+    println!("cargo:rerun-if-changed=package.json");
+    let output = Command::new("yarn")
+        .arg("install")
+        .output()
+        .expect("Failed to install dependencies");
+
+    if !output.status.success() {
+        io::stdout().write_all(&output.stderr).unwrap();
+    }
+
     println!("cargo:rerun-if-changed=frontend/");
 
-    Command::new("yarn")
+    let output = Command::new("yarn")
         .arg("build")
         .output()
         .expect("Failed to builed frontend");
+
+    if !output.status.success() {
+        io::stdout().write_all(&output.stderr).unwrap();
+    }
 }
+
