f7288d88df1973cb23c53c4750920d9668d81ec1
Ryan <beaconBrigade@gmail.com>
2022-07-09 19:14:03 -0230

    Update build.rs

--- build.rs
+++ build.rs
@@ -3,16 +3,6 @@ use std::io::{self, Write};
 
 // rebuild front-end
 fn main() {
-    println!("cargo:rerun-if-changed=package.json");
-    let output = Command::new("yarn")
-        .arg("install")
-        .output()
-        .expect("Failed to install dependencies");
-
-    if !output.status.success() {
-        io::stdout().write_all(&output.stderr).unwrap();
-    }
-
     println!("cargo:rerun-if-changed=frontend/");
 
     let output = Command::new("yarn")
